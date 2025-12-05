caf2ab358f9f703e51547178c28c554f6ee0e56c
Misha Dovhiy <44978117+mishadovhiy@users.noreply.github.com>
2025-12-05 22:08:47 +0200

    refactoring

---
+++ GalaryCloud/Service/PhotoPickerManager.swift
@@ -0,0 +1,110 @@
+//
+//  PhotoPickerManager.swift
+//  GalaryCloud
+//
+//  Created by Mykhailo Dovhyi on 05.12.2025.
+//
+
+import Foundation
+import Photos
+import Combine
+import UIKit
+
+class PHFetchManager: ObservableObject {
+    
+    @Published var assets: PHFetchResult<PHAsset>?
+    @Published var selectedIDs: [PHAsset] = []
+    
+    init() {
+        self.fetch()
+    }
+    
+    func fetch() {
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        assets = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+    }
+    
+    func select(asset: PHAsset) {
+        if selectedIDs.contains(asset) {
+            selectedIDs.removeAll(where: {
+                $0.localIdentifier == asset.localIdentifier
+            })
+        } else {
+            selectedIDs.append(asset)
+        }
+    }
+    
+    func saveToTemp(manager: FileManagerService,
+                    completion:@escaping()->()) {
+        DispatchQueue(label: "db", qos: .userInitiated).async {
+            if let asset = self.selectedIDs.first {
+                print(self.selectedIDs.count, " gtrfed ")
+                autoreleasepool {
+                    self.assetToData(asset) { [weak self] data in
+                        if let data {
+                            let format: String
+                            if #available(iOS 26.0, *) {
+                                format = PHAssetResource.assetResources(for: asset).first?.contentType.identifier ?? "jpg"
+                            } else {
+                                format = PHAssetResource.assetResources(for: asset).first?.uniformTypeIdentifier ?? "jpg"
+                            }
+                            manager.performSave(data: data, path: asset.localIdentifier.replacingOccurrences(of: "/", with: "") + "." + format, urlType: .temporary)
+                        }
+                        DispatchQueue.main.async {
+                            self?.selectedIDs.removeFirst()
+                            self?.saveToTemp(manager: manager, completion: completion)
+                        }
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completion()
+                }
+            }
+        }
+    }
+    
+    private func assetToData(
+        _ asset: PHAsset,
+        completion: @escaping(_ data: Data?)->()) {
+            guard let resource = PHAssetResource.assetResources(for: asset).first else {
+                return
+            }
+            let options = PHAssetResourceRequestOptions()
+            options.isNetworkAccessAllowed = true
+            let data = NSMutableData()
+            PHAssetResourceManager.default().requestData(
+                for: resource,
+                options: options,
+                dataReceivedHandler: {
+                    data.append($0)
+                }, completionHandler: { error in
+                    if error == nil {
+                        print(Thread.isMainThread, " yhrftgbvcghfjg ")
+                        completion(data as Data)
+                    } else {
+                        print(error, " tregfds ")
+                        completion(nil)
+                    }
+                })
+        }
+    
+    func fetchThumb(_ asset: PHAsset,
+                    completion: @escaping(_ image: UIImage?) -> ()) {
+        DispatchQueue(label: "thumb", qos: .userInitiated).async {
+            let imageManager = PHCachingImageManager()
+            let size: CGSize = .init(width: 60, height: 60)
+            imageManager.requestImage(for: asset, targetSize: size,
+                                      contentMode: .aspectFill,
+                                      options: nil) { image, _ in
+                let data = image?.jpegData(compressionQuality: 0.1)
+                let image = UIImage(data: data ?? .init())
+                DispatchQueue.main.async {
+                    completion(image?.changeSize(newWidth: 20))
+                }
+            }
+        }
+    }
+}
---
+++ GalaryCloud/View/Reusable/PhotoPickerSysView.swift
@@ -0,0 +1,169 @@
+//
+//  PhotoPickerSysView.swift
+//  GalaryCloud
+//
+//  Created by Mykhailo Dovhyi on 04.12.2025.
+//
+
+import SwiftUI
+import Combine
+import Photos
+
+struct PhotoPickerSysView: View {
+    
+    let completedSelection: ()->()
+    private let fileManager: FileManagerService = .init()
+    
+    @StateObject private var manager: PHFetchManager = .init()
+    @State private var assets: [Int: UIImage?] = [:]
+    @State private var selectedOnScreenIndxs: [Int] = []
+    @State var safeArea: EdgeInsets = .init()
+    @State var lastDroppedID: String?
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Spacer()
+                .frame(height: safeArea.top)
+            headerView
+            ScrollView(content: {
+                LazyVGrid(columns: (0..<4).compactMap({ _ in
+                        .init()
+                })) {
+                    galaryView
+                }
+            })
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.red)
+            Spacer()
+                .frame(height: safeArea.bottom)
+        }
+        .background(.black)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .modifier(ViewSizeReaderModifier(safeArea: $safeArea))
+    }
+    
+    var headerView: some View {
+        HStack {
+            Button("close") {
+                completedSelection()
+            }
+            .frame(maxHeight: .infinity)
+            .padding(.horizontal, 15)
+            Spacer()
+            Button("deselect \(manager.selectedIDs.count)") {
+                manager.selectedIDs.removeAll()
+            }
+            .frame(maxHeight: .infinity)
+            .padding(.horizontal, 15)
+            Button("save \(manager.selectedIDs.count)") {
+                manager.saveToTemp(manager: fileManager) {
+                    completedSelection()
+                }
+            }
+            .frame(maxHeight: .infinity)
+            .padding(.horizontal, 15)
+        }
+        .frame(height: 44)
+        .padding(.horizontal, 10)
+        .padding(.top, 50)
+        .background(.orange)
+    }
+    
+    func galaryImage(_ i: Int) -> some View {
+        GeometryReader(content: { proxy in
+            VStack {
+                if let image = self.assets[i],
+                   let image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                } else {
+                    LoaderView(isLoading: true)
+                        .frame(width: 30, height: 30)
+                }
+            }
+            .frame(width: proxy.size.width, height: proxy.size.height)
+            .clipped()
+        })
+        .aspectRatio(1, contentMode: .fill)
+        .clipped()
+        .overlay(content: {
+            if selectedOnScreenIndxs.contains(i) {
+                selectionIndicator
+            }
+        })
+    }
+    
+    var selectionIndicator: some View {
+        Color.red.opacity(0.3)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .allowsHitTesting(false)
+            .disabled(true)
+    }
+    
+    @ViewBuilder
+    var galaryView: some View {
+        ForEach(0..<(manager.assets?.count ?? 0), id: \.self) { i in
+            galaryImage(i)
+                .modifier(DragAndDropModifier(disabled: false, lastDroppedID: $lastDroppedID, itemID: "\(i)", didDrop: {
+                    select(i)
+                }))
+                .onTapGesture {
+                    select(i)
+                }
+                .onAppear(perform: {
+                    imageAppeared(i)
+                })
+                .onDisappear {
+                    imageDisapeared(i)
+                }
+                .onChange(of: manager.selectedIDs) { newValue in
+                    self.selectedListChanged(appearedAssetIndx: i)
+                }
+        }
+    }
+}
+
+fileprivate
+extension PhotoPickerSysView {
+    func select(_ i: Int) {
+        if let asset = manager.assets?.object(at: i)
+        {
+            manager.select(asset: asset)
+        }
+    }
+    
+    func imageAppeared(_ i: Int) {
+        if assets[i] == nil,
+           let asset = manager.assets?.object(at: i)
+        {
+            manager.fetchThumb(asset) { image in
+                self.assets.updateValue(image, forKey: i)
+            }
+            if manager.selectedIDs.contains(asset) {
+                selectedOnScreenIndxs.append(i)
+            }
+            
+        }
+    }
+    
+    func imageDisapeared(_ i: Int) {
+        selectedOnScreenIndxs.removeAll(where: {
+            $0 == i
+        })
+        assets.removeValue(forKey: i)
+    }
+    
+    func selectedListChanged(appearedAssetIndx i: Int) {
+        if let asset = manager.assets?.object(at: i)
+        {
+            if manager.selectedIDs.contains(asset) {
+                selectedOnScreenIndxs.append(i)
+            } else {
+                selectedOnScreenIndxs.removeAll(where: {
+                    $0 == i
+                })
+            }
+        }
+    }
+}
