5807e59fa9c13f5c3d927eef7958b10982f5697d
Misha Dovhiy <44978117+mishadovhiy@users.noreply.github.com>
2025-12-11 16:06:02 +0200

    watchos exeptions

--- GalaryCloud/Extension/UIImage.swift
+++ GalaryCloud/Extension/UIImage.swift
@@ -6,15 +6,42 @@
 //
 
 import UIKit
+import CoreGraphics
 
 extension UIImage {
     func changeSize(newWidth:CGFloat, from:CGSize? = nil, origin:CGPoint = .zero) -> UIImage {
         let widthPercent = newWidth / (from?.width ?? self.size.width)
         let proportionalSize: CGSize = .init(width: newWidth, height: widthPercent * (from?.height ?? self.size.height))
+#if !os(watchOS)
         let renderer = UIGraphicsImageRenderer(size: proportionalSize)
         let newImage = renderer.image { _ in
             self.draw(in: CGRect(origin: origin, size: proportionalSize))
         }
         return newImage
+#else
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        guard let context = CGContext(
+            data: nil,
+            width: Int(proportionalSize.width),
+            height: Int(proportionalSize.height),
+            bitsPerComponent: 8,
+            bytesPerRow: 0,
+            space: colorSpace,
+            bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+        ) else {
+            return .init()
+        }
+        
+        context.setFillColor(CGColor(red: 1, green: 0, blue: 0, alpha: 1))
+        context.fill(CGRect(origin: .zero, size: size))
+        
+        if let image = context.makeImage() {
+            return .init(cgImage: image)
+        } else {
+            return .init()
+
+        }
+#endif
     }
 }
